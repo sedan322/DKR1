0389c98cb55103dd391856b950099dedd5115105
sedan322 <117731030+sedan322@users.noreply.github.com>
2022-12-17 10:14:41 +0300

    Add files via upload

---
+++ Задание №1.pas
@@ -0,0 +1,15 @@
+﻿var y,h,x:real; a,b:integer;
+begin
+  h:=0.3;
+  a:=-9;
+  b:=3;
+  x:=a;
+  while x<=b do
+    begin
+    if x<-7 then y:=(x*x*x)/cos(x*2)
+    else if (x>=-7) and (x<1) then y:=(99/16)*(x/(x*x*x))
+    else if x>=1 then y:=(x*x)+(x*x);
+    writeln('x = ',x:1:2,' y = ',y:1:2);
+    x:=x+h;
+    end;
+end.
